f00c23c366f4337bee00a412f7f81a062ee33a72
vektorel <1muhammed.korucu@gmail.com>
2018-08-11 17:12:16 +0300

    Proje Defter 1

---
+++ app/src/main/java/com/example/vektorel/sifrelinotdefteri/ListeLer.java
@@ -0,0 +1,31 @@
+package com.example.vektorel.sifrelinotdefteri;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class ListeLer extends BaseAdapter {
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/vektorel/sifrelinotdefteri/notlar.java
@@ -0,0 +1,13 @@
+package com.example.vektorel.sifrelinotdefteri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class notlar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notlar);
+    }
+}
